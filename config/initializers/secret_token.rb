7c0a020c73ba699b828b8bde1ecbd5fed838656a
Pascal Jungblut <mail@pascal-jungblut.com>
2013-09-20 21:13:40 +0200

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Meh::Application.config.secret_key_base = 'a163b9376e598b57632585368ca32b7426a22c4c32cffe2f7dcedd43f3bdd367707a6527d033e0b6fd79915f3cba59a1a65de9ca736a257a45da538e850fd8cd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Meh::Application.config.session_store :cookie_store, key: '_meh_session'
